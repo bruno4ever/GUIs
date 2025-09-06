001238db703e6b41360db1e0f2a7bd94353a7384
Bruno S M <misterselectmice@gmail.com>
2025-09-06 16:30:08 -0300

    Update 02-anim.c

--- ex-1.3.1/02-anim.c
+++ ex-1.3.1/02-anim.c
@@ -14,7 +14,6 @@ int main(int argc, char* args[])
     const int inicio = 100;
     const int fim = 200;
 
-    // Quatro quadrados
     SDL_Rect q1 = { inicio, inicio, 20, 20 };  
     SDL_Rect q2 = { fim, inicio, 20, 20 };    
     SDL_Rect q3 = { fim, fim, 20, 20 };      
@@ -36,21 +35,25 @@ int main(int argc, char* args[])
         SDL_RenderPresent(ren);
         SDL_Delay(100);
 
+        // quadrado vermelho, se movimentando nessa ordem: direita, baixo, esquerda, cima. 
         if (q1.x < fim && q1.y == inicio) q1.x += 1;
         else if (q1.x >= fim && q1.y < fim) q1.y += 1;
         else if (q1.y >= fim && q1.x > inicio) q1.x -= 1;
         else if (q1.x <= inicio && q1.y > inicio) q1.y -= 1;
 
+       // quadrado verde, se movimentando nessa ordem: baixo, esquerda, cima, direita.
         if (q2.y < fim && q2.x == fim) q2.y += 1;
         else if (q2.y >= fim && q2.x > inicio) q2.x -= 1;
         else if (q2.x <= inicio && q2.y > inicio) q2.y -= 1;
         else if (q2.y <= inicio && q2.x < fim) q2.x += 1;
 
+      // quadrado azul, se movimentando nessa ordem: esquerda, cima, direita, baixo.
         if (q3.x > inicio && q3.y == fim) q3.x -= 1;
         else if (q3.x <= inicio && q3.y > inicio) q3.y -= 1;
         else if (q3.y <= inicio && q3.x < fim) q3.x += 1;
         else if (q3.x >= fim && q3.y < fim) q3.y += 1;
-
+     
+     // quadrado amarelo, se movimentando nessa ordem: cima, direita, baixo, esquerda
         if (q4.y > inicio && q4.x == inicio) q4.y -= 1;
         else if (q4.y <= inicio && q4.x < fim) q4.x += 1;
         else if (q4.x >= fim && q4.y < fim) q4.y += 1;
